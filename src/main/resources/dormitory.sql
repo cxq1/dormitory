2c7797fa794310a986fcaaf4b305be25bb234c31
cxq <1986541495@qq.com>
2020-02-13 12:37:44 +0800

    更新数据库

--- src/main/resources/dormitory.sql
+++ src/main/resources/dormitory.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50549
 File Encoding         : 65001
 
-Date: 2020-02-09 14:50:20
+Date: 2020-02-13 12:37:07
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -28,14 +28,21 @@ CREATE TABLE `power` (
   `status` tinyint(4) DEFAULT NULL COMMENT '是否可用',
   PRIMARY KEY (`id`),
   KEY `role_name` (`role_name`),
-  CONSTRAINT `power_ibfk_1` FOREIGN KEY (`role_name`) REFERENCES `role` (`role_name`)
+  CONSTRAINT `power_ibfk_1` FOREIGN KEY (`role_name`) REFERENCES `role` (`role_name`) ON DELETE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of power
 -- ----------------------------
 INSERT INTO `power` VALUES ('1111', 'admin', '/dormitory/power/list', '权限列表', '权限列表', '0');
-INSERT INTO `power` VALUES ('2222', 'admin', '/dormitory/power/add', '添加权限', '添加权限', '0');
+INSERT INTO `power` VALUES ('1227151820699262978', 'string11', '/dormitory/power/add', 'ttt', 'ddd', '0');
+INSERT INTO `power` VALUES ('1227152414092615681', 'admin', 'test', 'test', 'fdtet', '0');
+INSERT INTO `power` VALUES ('1227153024292544514', 'string11', '/dormitory/power/add', '添加tet', 'tetttttttt', '0');
+INSERT INTO `power` VALUES ('222', 'admin', 'fdsf', 'fsd', 'fds', '0');
+INSERT INTO `power` VALUES ('2222', 'admin', '/dormitory/power/add', '添加权限', 'string添加权限eeetttddsa', '0');
+INSERT INTO `power` VALUES ('33', 'admin', '/dormitory/power/add', '/dormitory/power/add', '/dormitory/power/add', '0');
+INSERT INTO `power` VALUES ('string', 'admin', 'string', 'string', 'string', '0');
+INSERT INTO `power` VALUES ('string111', 'admin', 'string', 'string', 'string', '1');
 
 -- ----------------------------
 -- Table structure for reserve
@@ -80,6 +87,9 @@ CREATE TABLE `role` (
 -- Records of role
 -- ----------------------------
 INSERT INTO `role` VALUES ('1111', '所有权限', 'admin', '管理员权限', '0');
+INSERT INTO `role` VALUES ('1227474925128835074', 'tete', 'test', 'test', '0');
+INSERT INTO `role` VALUES ('2222', 'string111', 'string11', 'string', '0');
+INSERT INTO `role` VALUES ('333', 'string', 'stest', 'fff', '0');
 
 -- ----------------------------
 -- Table structure for roomcost
@@ -140,7 +150,7 @@ CREATE TABLE `user` (
   `mail` char(32) NOT NULL,
   `number` char(18) NOT NULL,
   `role_name` char(15) NOT NULL DEFAULT '',
-  `status` tinyint(4) DEFAULT NULL COMMENT '是否可用',
+  `status` tinyint(4) DEFAULT '0' COMMENT '是否可用',
   `salt` varchar(255) DEFAULT NULL COMMENT '盐值',
   `gmt_created_time` datetime DEFAULT NULL,
   `gmt_modify_time` datetime DEFAULT NULL,
@@ -150,3 +160,21 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
+INSERT INTO `user` VALUES ('111', 'cxq', 'cxq', '123', '男', '13676562115', '198@qq.com', '201731960115', 'admin', '0', '', '2020-02-12 14:34:11', '2020-02-12 14:34:15');
+INSERT INTO `user` VALUES ('222', 'test', 'teste', '111', '女', '2222', '434@qq.com', '1111', 'admin', '0', '', '2020-02-12 14:55:22', '2020-02-12 20:21:57');
+INSERT INTO `user` VALUES ('435', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '1', '0', '', '2020-02-12 15:22:35', '2020-02-12 20:21:37');
+INSERT INTO `user` VALUES ('4444', 'string', 'tes', 'string', '男', 'string', 'string', 'string', 'string', '0', 'string', '2020-02-12 15:22:35', '2020-02-12 20:10:45');
+INSERT INTO `user` VALUES ('4643', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
+INSERT INTO `user` VALUES ('53463', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
+INSERT INTO `user` VALUES ('5463', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
+INSERT INTO `user` VALUES ('54645', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
+INSERT INTO `user` VALUES ('546456', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
+INSERT INTO `user` VALUES ('54654', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
+INSERT INTO `user` VALUES ('6475', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
+INSERT INTO `user` VALUES ('65437', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
+INSERT INTO `user` VALUES ('654675', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
+INSERT INTO `user` VALUES ('6547', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
+INSERT INTO `user` VALUES ('65634', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
+INSERT INTO `user` VALUES ('6565', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
+INSERT INTO `user` VALUES ('745', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
+INSERT INTO `user` VALUES ('7567', 'test', 'teste', '111', '男', '2222', '434@qq.com', '1111', '', '0', '', '2020-02-12 15:22:35', '2020-02-12 15:22:38');
